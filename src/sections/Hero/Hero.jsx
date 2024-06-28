8416692c38802d427adac8d4298d0cb587dd0c2e
Nicholas Wong <nicholasw1688@yahoo.com>
2024-06-27 22:40:51 -0400

    added ThemeContext with light and dark mode f

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,11 +1,21 @@
 import styles from './HeroStyles.module.css';
 import heroImg from '../../assets/hero-img.png';
-import themeIcon from '../../assets/sun.svg';
-import linkedinIcon from '../../assets/linkedin-light.svg';
-import githubIcon from '../../assets/github-light.svg';
+import sun from '../../assets/sun.svg';
+import moon from '../../assets/moon.svg';
+import linkedinLight from '../../assets/linkedin-light.svg';
+import linkedinDark from '../../assets/linkedin-dark.svg';
+import githubLight from '../../assets/github-light.svg';
+import githubDark from '../../assets/github-dark.svg';
 import CV from '../../assets/cv.pdf';
+import { useTheme } from '../../common/ThemeContext'
 
 function Hero() {
+  const { theme, toggleTheme } = useTheme()
+
+  const themeIcon = theme === 'light' ? sun : moon;
+  const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+  const githubIcon = theme === 'light' ? githubLight : githubDark;
+
   return <section if="hero" className={styles.container}>
     <div className={styles.colorModeContainer}>
         <img 
@@ -17,6 +27,7 @@ function Hero() {
           className={styles.colorMode}
           src={themeIcon}
           alt="Color mode icon"
+          onClick={toggleTheme}
         />
     </div>
     <div className={styles.info}>

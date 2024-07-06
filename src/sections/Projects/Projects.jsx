d3763f8f2433aeeba5074852c716d1af6f00f74a
Nicholas Wong <nicholasw1688@yahoo.com>
2024-07-06 12:32:00 -0400

    changed image descriptions

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import styles from './ProjectsStyles.module.css'
+import wordle from '../../assets/wordle.png'
 
 function Projects() {
   return (
-    <div>Projects</div>
+    <section id="projects" className={styles.container}>
+      <h1 className='sectionTitle'>Projects</h1>
+      <div className={styles.projectsContainer}>
+        <a href="https://github.com/nickw0ng/Wordle"
+        target="_blank">
+          <img className="hover" src={wordle} alt="Viberr logo" />
+          <h3>Wordle</h3>
+          <p>Word Puzzle Game</p>
+        </a>
+      </div>
+    </section>
   )
 }
 

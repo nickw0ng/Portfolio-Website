ff51d914d9f6a8a85c69072140da035e834d6dac
Nicholas Wong <nicholasw1688@yahoo.com>
2024-07-07 22:32:14 -0400

    add more projects

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,20 +1,41 @@
 import styles from './ProjectsStyles.module.css'
 import wordle from '../../assets/wordle.png'
+import ProjectCard from '../../common/ProjectCard';
+import klondike from '../../assets/klondike.png'
+import video from '../../assets/video.png'
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
-      <h1 className='sectionTitle'>Projects</h1>
+      <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
-        <a href="https://github.com/nickw0ng/Wordle"
-        target="_blank">
-          <img className="hover" src={wordle} alt="Viberr logo" />
-          <h3>Wordle</h3>
-          <p>Word Puzzle Game</p>
-        </a>
+        <ProjectCard
+          src={video}
+          link="https://github.com/nickw0ng/Wordle"
+
+          h3="Video Manager"
+          p="RESTful API using Flask"
+        />
+
+        <ProjectCard
+          src={wordle}
+          link="https://github.com/nickw0ng/Wordle"
+
+          h3="Wordle"
+          p="Puzzle Game"
+        />
+
+        <ProjectCard
+          src={klondike}
+          link="https://github.com/nickw0ng/Klondike"
+
+          h3="Klondike (Solitaire)"
+          p="Card Game"
+        />
       </div>
     </section>
   )
 }
 
 export default Projects
+

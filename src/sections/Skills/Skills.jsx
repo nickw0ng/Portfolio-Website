f9b49434d0f80bd41333aa7ec3a82e8f034e181d
Nicholas Wong <nicholasw1688@yahoo.com>
2024-07-08 23:02:51 -0400

    added typescript

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -15,7 +15,7 @@ function Skills() {
         <SkillList src={checkMarkIcon} skill="Java" />
         <SkillList src={checkMarkIcon} skill="Python" />
         <SkillList src={checkMarkIcon} skill="JavaScript" />
-        <SkillList src={checkMarkIcon} skill="TypeScr" />
+        <SkillList src={checkMarkIcon} skill="TypeScript" />
       </div>
       <hr />
       <div className={styles.skillList}>

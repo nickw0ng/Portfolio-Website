609eae0aecad78dab3828e8a8d4edf4c7fa3b419
Nicholas Wong <nicholasw1688@yahoo.com>
2024-07-08 23:00:13 -0400

    skills section

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -1,10 +1,39 @@
 import styles from './SkillsStyles.module.css';
+import checkMarkIconDark from '../../assets/checkmark-dark.svg';
+import checkMarkIconLight from '../../assets/checkmark-light.svg';
+import SkillList from '../../common/SkillList';
+import { useTheme } from '../../common/ThemeContext';
 
 function Skills() {
-  return <section id="skills" className={styles.container}>
-    Skills
-  </section>
-  
+  const { theme } = useTheme();
+  const checkMarkIcon = theme === 'light' ? checkMarkIconLight : checkMarkIconDark;
+
+  return (
+    <section id="skills" className={styles.container}>
+      <h1 className="sectionTitle">Skills</h1>
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Java" />
+        <SkillList src={checkMarkIcon} skill="Python" />
+        <SkillList src={checkMarkIcon} skill="JavaScript" />
+        <SkillList src={checkMarkIcon} skill="TypeScript" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="React" />
+        <SkillList src={checkMarkIcon} skill="Node.js" />
+        <SkillList src={checkMarkIcon} skill="Flask" />
+        <SkillList src={checkMarkIcon} skill="SQLite" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Redux" />
+        <SkillList src={checkMarkIcon} skill="Webpack" />
+        <SkillList src={checkMarkIcon} skill="Git" />
+        <SkillList src={checkMarkIcon} skill="Jest" />
+        <SkillList src={checkMarkIcon} skill="Bootstrap" />
+      </div>
+    </section>
+  );
 }
 
-export default Skills
+export default Skills;

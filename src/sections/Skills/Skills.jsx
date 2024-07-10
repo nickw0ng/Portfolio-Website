cca21c3785bd78109d0d45a23aec1d4fb1fe11e6
Nicholas Wong <nicholasw1688@yahoo.com>
2024-07-09 20:26:25 -0400

    undo

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -16,7 +16,6 @@ function Skills() {
         <SkillList src={checkMarkIcon} skill="JavaScript" />
         <SkillList src={checkMarkIcon} skill="Java" />
         <SkillList src={checkMarkIcon} skill="TypeScript" />
-        <SkillList src={checkMarkIcon} skill="TypeScript" />
       </div>
       <hr />
       <div className={styles.skillList}>
